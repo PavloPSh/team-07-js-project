0ffb60d1a0d156782b442a09442c237a2a5ae8f7
Pavlo Psheborovskyi <pavlopshe@gmail.com>
2022-08-24 13:54:48 +0300

    Update headerBtns.js

--- src/js/headerBtns.js
+++ src/js/headerBtns.js
@@ -3,14 +3,19 @@ const libBtn = document.querySelector('button[data-action="toLibrary"]');
 const iconBtn = document.querySelector('.icon-button');
 
 iconBtn.addEventListener('click', () =>{
-    window.location.href = '/index.html'
+    // window.location.href = '/index.html'
+    document.location.assign('/index.html');
 })
 
 homeBtn.addEventListener('click', () => {
-    window.location.href = '/index.html'
+    // window.location.href = '/index.html'
+    document.location.assign('/index.html');
+
 })
 
 libBtn.addEventListener('click', () => {
-    window.location.href = '/library.html' 
+    // window.location.href = '/library.html'
+    document.location.assign('/library.html');
+
 })
 
